fa4a84e9b061dab6962dbba0bcd74c0b1e39b537
Guilherme Mattos <guilhermevsmattos@outlook.com>
2026-03-04 17:43:40 -0300

    feat: add list workout plans route and validation schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -123,6 +123,41 @@ export const WorkoutPlanDetailsResponseSchema = z.object({
   ),
 });
 
+export const ListWorkoutPlansQuerySchema = z.object({
+  active: z
+    .enum(["true", "false"])
+    .transform((v) => v === "true")
+    .optional(),
+});
+
+export const ListWorkoutPlansSchema = z.array(
+  z.object({
+    id: z.uuid(),
+    name: z.string(),
+    isActive: z.boolean(),
+    workoutDays: z.array(
+      z.object({
+        id: z.uuid(),
+        name: z.string(),
+        weekDay: z.enum(WeekDay),
+        isRest: z.boolean(),
+        estimatedDurationInSeconds: z.number(),
+        coverImageUrl: z.url().optional(),
+        exercises: z.array(
+          z.object({
+            id: z.uuid(),
+            order: z.number(),
+            name: z.string(),
+            sets: z.number(),
+            reps: z.number(),
+            restTimeInSeconds: z.number(),
+          }),
+        ),
+      }),
+    ),
+  }),
+);
+
 export const WorkoutDayDetailsParamsSchema = z.object({
   workoutPlanId: z.uuid(),
   workoutDayId: z.uuid(),
